17735dacad814e40d3af8a1811132ee42a8709ce
olekserbinov <oleksandr.serbinov@stepico.com>
2025-03-08 11:34:45 +0200

    done, before docker setup

--- config/config.php
+++ config/config.php
@@ -8,7 +8,8 @@ declare(strict_types=1);
 class Config
 {
     // Замінено динамічний вираз на статичний метод
-    public const TRADE_SERVER_URL_DEFAULT = 'http://195.7.7.93:18080';
+    public const TRADE_SERVER_URL_DEFAULT = 'http://195.7.7.93:18080'; // dev server
+    // const TRADE_SERVER_URL = 'http://164.68.117.90:18080';   // demo server
     
     /**
      * Отримання URL торгового сервера
